ec864a7ef1f4f5d64bb37df6e1dca30c43ef1830
Sebastian Pietrykowski <s_pietrykowski@outlook.com>
2023-07-20 11:13:52 +0200

    Eslint and Prettier basic configuration
    Eslint and Prettier basic configuration

--- skill-matrix/src/app/app-routing.module.ts
+++ skill-matrix/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+// eslint-disable-next-line sort-imports
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
