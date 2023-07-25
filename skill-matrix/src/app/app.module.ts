b920469703096288a6a2af8eb525770b296ce5fa
Sebastian Pietrykowski <s_pietrykowski@outlook.com>
2023-07-25 15:10:55 +0200

    Added forms for Employee

--- skill-matrix/src/app/app.module.ts
+++ skill-matrix/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { EmployeeComponent } from './features/components/employee/employee.component';
+import { EmployeeDetailComponent } from './features/components/employee-detail/employee-detail.component';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import { EmployeeDetailComponent } from './features/components/employee-detail/employee-detail.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, EmployeeComponent, EmployeeDetailComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- skill-matrix/src/app/features/components/employee-detail/employee-detail.component.ts
+++ skill-matrix/src/app/features/components/employee-detail/employee-detail.component.ts
@@ -1,4 +1,12 @@
 import { Component, Input } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validator,
+  Validators,
+} from '@angular/forms';
 import { Employee } from '../../models/employee';
 
 @Component({
@@ -8,4 +16,64 @@ import { Employee } from '../../models/employee';
 })
 export class EmployeeDetailComponent {
   @Input() employee?: Employee;
+
+  employeeProfileForm = this.formBuilder.group({
+    id: [this.employee?.id],
+    name: ['', Validators.required],
+    surname: ['', Validators.required],
+    employmentDate: ['', Validators.required],
+    listOfSkills: this.formBuilder.array([]),
+    listOfProjects: this.formBuilder.array([]),
+    manager: [''],
+  });
+
+  get listOfProjects() {
+    return this.employeeProfileForm.get('listOfProjects') as FormArray;
+  }
+
+  get listOfSkills() {
+    return this.employeeProfileForm.get('listOfSkills') as FormArray;
+  }
+
+  addUndefinedProject(): void {
+    const length = this.listOfSkills.length;
+    if (
+      this.listOfProjects.length < 1 ||
+      this.listOfProjects.at(length - 1).value != ''
+    )
+      this.listOfProjects.push(this.formBuilder.control(''));
+  }
+
+  removeProjectAt(index: number): void {
+    this.listOfProjects.removeAt(index);
+  }
+
+  addUndefinedSkill(): void {
+    if (
+      this.listOfSkills.length < 1 ||
+      this.listOfSkills.at(length - 1).value != ''
+    )
+      this.listOfSkills.push(this.formBuilder.control(''));
+  }
+
+  removeSkillAt(index: number): void {
+    this.listOfSkills.removeAt(index);
+  }
+
+  updateProfile(): void {
+    this.employeeProfileForm.patchValue({
+      name: '',
+      surname: '',
+      employmentDate: '',
+      // listOfSkills: {
+      //   'skill 1', 'skill 2',
+      // }
+      // listOfProjects: {
+      //   'project 1', 'project 2',
+      // }
+      manager: '',
+    });
+  }
+
+  constructor(private formBuilder: FormBuilder) {}
 }
