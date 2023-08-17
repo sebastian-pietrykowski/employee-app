e5fe1ff82a68b577c8854f097d64fefc9f1c8bbb
Sebastian Pietrykowski <s_pietrykowski@outlook.com>
2023-08-17 15:32:02 +0200

    Added more tests for EmployeeComponent

--- skill-matrix/src/app/features/employee/employee.component.spec.ts
+++ skill-matrix/src/app/features/employee/employee.component.spec.ts
@@ -1,8 +1,20 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EmployeeComponent } from './employee.component';
+import { EmployeeSearchComponent } from './components/employee-search/employee-search.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslateTestingModule } from 'ngx-translate-testing';
+
+const ENGLISH_TRANSLATIONS_MOCK = {
+  employee: {
+    header: 'Employee info',
+  },
+};
 
 describe('EmployeeComponent', () => {
   let component: EmployeeComponent;
@@ -11,11 +23,17 @@ describe('EmployeeComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
+        BrowserAnimationsModule,
         HttpClientTestingModule,
+        MatAutocompleteModule,
+        MatButtonModule,
+        MatInputModule,
         MatProgressSpinnerModule,
-        TranslateModule.forRoot(),
+        TranslateTestingModule.withTranslations({
+          en: ENGLISH_TRANSLATIONS_MOCK,
+        }),
       ],
-      declarations: [EmployeeComponent],
+      declarations: [EmployeeComponent, EmployeeSearchComponent],
       providers: [],
     });
     fixture = TestBed.createComponent(EmployeeComponent);
@@ -26,4 +44,20 @@ describe('EmployeeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render title', inject(
+    [TranslateService],
+    (translateService: TranslateService) => {
+      const expectedTitle = ENGLISH_TRANSLATIONS_MOCK.employee.header;
+      translateService.setDefaultLang('en');
+      fixture.componentInstance.isLoading = false;
+      fixture.detectChanges();
+
+      const compiled = fixture.nativeElement as HTMLElement;
+      const actualTitle =
+        compiled.querySelector('.employee__header')?.textContent;
+
+      expect(actualTitle).toBe(expectedTitle);
+    },
+  ));
 });
