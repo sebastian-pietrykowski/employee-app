85a1906ed430ba3ba41d8a8160e22f3e2e6a654a
Sebastian Pietrykowski <s_pietrykowski@outlook.com>
2023-08-31 14:09:38 +0200

    Changed theme

--- skill-matrix/src/app/features/employee/employee.module.ts
+++ skill-matrix/src/app/features/employee/employee.module.ts
@@ -4,6 +4,7 @@ import { EmployeeRoutingModule } from './employee-routing.module';
 import { EmployeeSearchComponent } from './components/employee-search/employee-search.component';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { NgModule } from '@angular/core';
@@ -19,6 +20,7 @@ import { TranslateModule } from '@ngx-translate/core';
     MatProgressSpinnerModule,
     MatInputModule,
     MatAutocompleteModule,
+    MatCardModule,
   ],
 })
 export class EmployeeModule {}
