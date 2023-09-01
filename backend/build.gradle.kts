7a7066b87c1b76f985ea042fb3a23d0e65b256fb
Sebastian Pietrykowski <s_pietrykowski@outlook.com>
2023-09-01 13:05:25 +0200

    Added tests for EmployeeService

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -26,12 +26,15 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    testImplementation("org.projectlombok:lombok")
+    testImplementation("org.projectlombok:lombok")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.projectlombok:lombok")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.h2database:h2:2.2.220")
 }
 
 tasks.withType<Test> {
